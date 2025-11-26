4d091eafd395d34270781a3e894be3e0d5d0ce05
TinaTzeng <tina870916@gmail.com>
2025-11-26 12:20:27 +0800

    update sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+CREATE TABLE insurance_embeddings (
+    id TEXT PRIMARY KEY,
+    text TEXT,
+    context_text TEXT,
+    policy_name TEXT,
+    metadata_file TEXT,
+    type TEXT,
+    embedding vector(768)
+);
